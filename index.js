f7150b1c43781ac602ed0fb26e5a71bb8b850b7b
likhona7 <likhona@projectcodex.co>
2017-01-13 06:44:30 +0200

    likhona

--- index.js
+++ index.js
@@ -31,6 +31,16 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+
+
+
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 })
